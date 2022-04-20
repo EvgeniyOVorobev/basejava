182af8d09d8b700591484e3fbd35e603ce8ed908
Evgeniy Vorobev <vorobiev71@yandex.ru>
2022-04-20 14:34:44 +0300

    HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,86 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    static int x = 0;
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
+
     void save(Resume r) {
+        storage[x] = r;
+        x++;
+
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume resume = new Resume();
+        for (Resume value : storage) {
+            if (value != null) {
+                if (uuid.equalsIgnoreCase(value.toString())) {
+                    resume = value;
+                    break;
+                }
+            }
+
+        }
+
+        return resume;
     }
 
     void delete(String uuid) {
+        Resume[] resumes = null;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                resumes = new Resume[storage.length - 1];
+                for (int x = 0; x < i; x++) {
+                    resumes[x] = storage[x];
+                }
+                for (int j = i; j < storage.length - 1; j++) {
+                    resumes[j] = storage[j + 1];
+                }
+                break;
+            }
+        }
+        storage = resumes;
+
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resume = null;
+        int i = 0;
+        for (int j = 0; j < storage.length; j++) {
+            if (storage[j] != null) {
+                i++;
+            }
+
+        }
+        resume = new Resume[i];
+        for (int k = 0; k < resume.length; k++) {
+            resume[k] = storage[k];
+
+        }
+
+
+        return resume;
     }
 
     int size() {
-        return 0;
+        Resume resume = new Resume();
+        int i = 0;
+        for (int j = 0; j < storage.length; j++) {
+            if (storage[j] != null)
+                i++;
+        }
+        return i;
     }
 }
